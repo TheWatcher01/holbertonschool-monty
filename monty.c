321f188734b72bdcb93d0aafc84698de403de083
TheWatcher01 <teddydeberdt@gmail.com>
2024-01-03 16:04:19 +0100

    add opcode.c file function and implement it every

---
+++ monty.c
@@ -0,0 +1,50 @@
+#include "monty.h"
+
+/**
+ * main - Main function to interpret Monty byte codes.
+ *
+ * @argc: Argument count.
+ * @argv: Argument vector.
+ *
+ * Function opens Monty byte code file, reads each line,
+ * and executes opcode on each line.
+ * If file cannot be opened, or if unknown instruction is encountered,
+ * it prints error message and exits with status EXIT_FAILURE.
+ */
+
+int main(int argc, char *argv[])
+{
+	FILE *file;
+	char *line, *opcode;
+	size_t len = 0;
+	ssize_t read;
+	unsigned int line_number = 0;
+	stack_t *stack = NULL;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r");
+	if (file = NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while ((read = getline(&line, &len, file)) != -1)
+	{
+		line_number++;
+		opcode = strtok(line, " \n\t\r");
+		if (opcode != NULL && opcode[0] != '#')
+				opcode_handle(opcode, &stack, line_number);
+	}
+
+fclose(file);
+if (line)
+	free(line);
+free_stack(stack);
+return (EXIT_SUCCESS);
+}
---
+++ opcode.c
@@ -0,0 +1,41 @@
+#include "monty.h"
+
+/**
+ * opcode_handle - Handles the opcode
+ * @opcode: The opcode to handle
+ * @stack: Double pointer to the top of the stack
+ * @line_number: The line number
+ *
+ * Description: This function handles the opcode. It checks if the opcode
+ * matches any of the predefined ones and if so, calls the corresponding
+ * function. If the opcode does not match any of the predefined ones,
+ * it prints an error message and exits the program.
+ */
+
+void opcode_handle(char *opcode, stack_t **stack, unsigned int line_number)
+{
+    instruction_t instruction[] = {
+        {"push", push},
+        {"pall", pall},
+        {"pint", pint},
+        {"pop", pop},
+        {"swap", swap},
+        {"add", add},
+        {"nop", nop},
+        {NULL, NULL}
+    };
+    int i = 0;
+
+    while (instruction[i].opcode != NULL)
+    {
+        if (strcmp(opcode, instruction[i].opcode) == 0)
+        {
+            instruction[i].f(stack, line_number);
+            return;
+        }
+        i++;
+    }
+
+    fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+    exit(EXIT_FAILURE);
+}
