9b928df2bd77477ddc82dffbe38c100caa797e18
root <root@DESKTOP-1V1HPI1>
2024-01-04 10:00:22 +0100

    add pint and few fix

--- monty.c
+++ monty.c
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
 	}
 
 	file = fopen(argv[1], "r");
-	if (file = NULL)
+	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
