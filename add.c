6ceb6c08e8c708d4bb9c3cce71864a69eced004a
Adolberton <7557@holbertonstudents.com>
2024-01-04 18:00:03 +0100

    fix stderr & indent

--- add.c
+++ add.c
@@ -8,12 +8,12 @@
 
 void add(stack_t **stack, unsigned int line_number)
 {
-    if (!stack || !*stack || !(*stack)->next)
-    {
-        fprintf(stderr, "L%d: cant't add, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (!stack || !*stack || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    (*stack)->next->n += (*stack)->n;
-    pop(stack, line_number);
+	(*stack)->next->n += (*stack)->n;
+	pop(stack, line_number);
 }
