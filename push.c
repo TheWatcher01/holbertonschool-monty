86bcba16f104c6b17698b223216eb04a261c2c18
Adolberton <7557@holbertonstudents.com>
2024-01-04 18:21:37 +0100

    check strtok err

--- push.c
+++ push.c
@@ -12,29 +12,34 @@
 
 void push(stack_t **stack, unsigned int line_number)
 {
-    stack_t *new_node;
-    char *n_str;
-    int n;
+	stack_t *new_node;
+	char *n_str;
+	int n;
 
-    n_str = strtok(NULL, " \n\t\r");
-    if (n_str == NULL || check_if_number(n_str) == 0)
-    {
-        fprintf(stderr, "L%d: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    n = atoi(n_str);
+	n_str = strtok(NULL, " \n\t\r");
+	if (n_str == NULL || check_if_number(n_str) == 0)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (strtok(NULL, " \n\t\r") != NULL)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	n = atoi(n_str);
 
-    new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed in line %u\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed in line %u\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = *stack;
-    if (*stack != NULL)
-        (*stack)->prev = new_node;
-    *stack = new_node;
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
+	*stack = new_node;
 }
